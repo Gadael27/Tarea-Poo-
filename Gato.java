c68b127aa1f0d3ea278879fa2cac0099c9d75df6
Gadael <119815893+Gadael27@users.noreply.github.com>
2023-02-23 10:42:48 -0600

    Add files via upload
    Tarea Gato

---
+++ Gato.java
@@ -0,0 +1,6 @@
+public class Gato{
+    public static void main(String[] args) {
+        String gato = " ------\n|      |\n|      |\n|      |\n ------";
+        System.out.println(gato);
+    }
+}
